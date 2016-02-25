a182e531a44c620acbc9122ad40934ee5ba66080
Volker Thiel <volker.thiel@mage-profis.de>
2016-02-25 16:26:56 +0100

    Small improvements in overall code quality, added namespaces (mpheadercart vs. headercart) for models, blocks, helpers

--- src/app/code/local/MageProfis/HeaderCart/Block/Cart.php
+++ src/app/code/local/MageProfis/HeaderCart/Block/Cart.php
@@ -1,11 +1,15 @@
 <?php
-class MageProfis_HeaderCart_Block_Cart extends Mage_Checkout_Block_Cart_Sidebar 
+class MageProfis_HeaderCart_Block_Cart extends Mage_Checkout_Block_Cart_Sidebar
 {
+    /**
+     * Get title for header cart block
+     *
+     * @return string
+     */
     protected function getCartTitle()
     {
         $config_title = Mage::getStoreConfig('headercart/general/title');
-        if($config_title != "")
-        {
+        if ($config_title != "") {
             return $config_title;
         }
         return $this->__('Cart');
